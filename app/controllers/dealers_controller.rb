bd0296b3c1b221e5cb1825c89cc438425384e1f2
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-09-16 15:57:04 +0300

    style for dealers

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -1,25 +1,20 @@
 class DealersController < ApplicationController
   before_action :set_dealer, only: %i[ show edit update destroy ]
 
-  # GET /dealers
   def index
     @dealers = Dealer.all
   end
 
-  # GET /dealers/1
   def show
   end
 
-  # GET /dealers/new
   def new
     @dealer = Dealer.new
   end
 
-  # GET /dealers/1/edit
   def edit
   end
 
-  # POST /dealers
   def create
     @dealer = Dealer.new(dealer_params)
 
@@ -30,7 +25,6 @@ class DealersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /dealers/1
   def update
     if @dealer.update(dealer_params)
       redirect_to @dealer, notice: "Dealer was successfully updated."
@@ -39,19 +33,16 @@ class DealersController < ApplicationController
     end
   end
 
-  # DELETE /dealers/1
   def destroy
     @dealer.destroy
     redirect_to dealers_url, notice: "Dealer was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_dealer
       @dealer = Dealer.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def dealer_params
       params.require(:dealer).permit(:name)
     end
