102052d968776b1bfde69bc3309f66e63dbe69de
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-10-17 00:40:28 +0300

    add tests to model Brand

--- spec/factories/brands.rb
+++ spec/factories/brands.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :brand do
-    name { 'BMW' }
+    name { 'bmw' }
+  end
+
+  factory :random_brand, class: 'Brand' do
+    name { Faker::Vehicle.manufacture }
+    description { Faker::Lorem.paragraph }
   end
 end
--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
-  it 'has a unique name' do
-    brand1 = create(:brand, name: 'BMW')
-    brand2 = build(:brand, name: 'bmw')
-    expect(brand2).not_to be_valid
+  it 'validate presence of name' do
+    brand = build(:brand, name: '')
+
+    expect(brand).not_to be_valid
   end
+
+  it 'validate uniqueness of name' do
+    brand_1 = create(:brand, name: 'bmw')
+    brand_2 = build(:brand, name: 'bmw')
+
+    expect(brand_2).not_to be_valid
+  end
+
+  it 'is expected to be upcase name' do
+    brand = create(:brand, name: 'bmw')
+
+    expect(brand.name).to eq('BMW')
+  end
+
+  it { is_expected.to have_many(:models).dependent(:destroy) }
 end
