92e17edf115368b29f1ae510a9026d18d9fe2c63
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-10-17 21:00:54 +0300

    add tests to model Model

---
+++ spec/factories/submodels.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :submodel do
+    name { '435' }
+    model
+  end
+end
--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Model, type: :model do
-  it 'has a fill name' do
-    model = build(:model, name: '')
-    expect(model).not_to be_valid
-  end
+  subject { build(:model) }
+
+  it { is_expected.to belong_to(:brand) }
+  it { is_expected.to have_many(:cars).dependent(:destroy) }
+  it { is_expected.to have_many(:submodels).dependent(:destroy) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
