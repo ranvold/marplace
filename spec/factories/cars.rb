5a4fe5446543c53423344d5c73f7ea5156cc10bf
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-10-18 00:21:00 +0300

    add tests to model Car

---
+++ spec/factories/cars.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :car do
+    model
+    dealer
+  end
+end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:model) }
+  it { is_expected.to belong_to(:dealer) }
+  it { is_expected.to have_many(:favorites).dependent(:destroy) }
+  it { is_expected.to have_many(:users).through(:favorites) }
+  it { is_expected.to have_many_attached(:images) }
+  it { is_expected.to define_enum_for(:fuel_type) }
+  it { is_expected.to define_enum_for(:body_color) }
+  it { is_expected.to define_enum_for(:gearbox) }
 end
