18f38ed35ebb97874bfed6867a8e21ff929d4070
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-10-18 19:16:59 +0300

    add tests to Seller and User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:favorites).dependent(:destroy) }
+  it { is_expected.to have_many(:cars).through(:favorites) }
 end
