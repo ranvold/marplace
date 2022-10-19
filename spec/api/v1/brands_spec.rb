845718a147e1f0f27f170476bd1da7d5822b2883
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-10-19 16:08:06 +0300

    refactoring

--- spec/api/v1/brands_spec.rb
+++ spec/api/v1/brands_spec.rb
@@ -1,27 +1,19 @@
 require 'rails_helper'
 
 describe V1::Brands do
-  let(:brand) { create :brand }
-
-  describe 'GET /brands' do
-    it 'returns success code' do
-      get '/api/v1/brands'
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'returns all brands' do
-      brands = Brand.all
-      get '/api/v1/brands'
-      expect(JSON.parse(response.body)).to eq(JSON.parse(brands.to_json))
-    end
+  it 'returns success code' do
+    get '/api/v1/brands'
+    expect(response).to have_http_status(:ok)
   end
 
-  describe 'GET /brands/:id' do
-    let(:brand) { create :brand }
+  it 'returns a brand by id' do
+    brand = create(:brand)
+    get "/api/v1/brands/#{brand.id}"
+    expect(JSON.parse(response.body)).to eq(JSON.parse(brand.to_json))
+  end
 
-    it 'returns 404 for an invalid id' do
-      get '/api/v1/brands/500'
-      expect(response).to have_http_status(:not_found)
-    end
+  it 'returns not found for an invalid id' do
+    get '/api/v1/brands/500'
+    expect(response).to have_http_status(:not_found)
   end
 end
